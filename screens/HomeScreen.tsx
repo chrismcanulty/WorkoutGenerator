9c41f6cc55b8d182e2e21ad5c1409297453bf814
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-03-20 23:27:05 -0700

    added simple test stylings and button component to home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
-import {View} from 'react-native';
+import {Button, View} from 'react-native';
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import styled from 'styled-components/native';
@@ -7,6 +7,12 @@ import styled from 'styled-components/native';
 export default function HomeScreen({navigation}: NativeStackHeaderProps) {
   const Header = styled.Text`
     font-family: 'Montserrat-Regular';
+    font-size: 26px;
+    margin: 35px;
+    padding: 10px;
+    border: 5px solid grey;
+    border-radius: 5px;
+    text-align: center;
   `;
 
   useEffect(() => {
@@ -15,7 +21,8 @@ export default function HomeScreen({navigation}: NativeStackHeaderProps) {
 
   return (
     <View>
-      <Header>I am home screen</Header>
+      <Header>Workout Generator</Header>
+      <Button title="Press me" onPress={() => alert('Simple Button pressed')} />
     </View>
   );
 }
