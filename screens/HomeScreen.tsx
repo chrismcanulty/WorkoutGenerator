90ecd6b9d1e87fc44f4dc5ec158ebe9b7476ef92
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-03-21 13:32:39 -0700

    fixed linting issues not caught by prettier

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,32 +1,32 @@
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
-import {Button, View} from 'react-native';
+import {View} from 'react-native';
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import styled from 'styled-components/native';
 
 export default function HomeScreen({navigation}: NativeStackHeaderProps) {
-  const Header = styled.Text`
-    font-family: 'Montserrat-Regular';
-    font-size: 35px;
-    margin: 35px;
-    margin-top: 70px;
-    padding: 10px;
-    border: 5px solid grey;
-    border-radius: 5px;
-    text-align: center;
-  `;
   const Button = styled.TouchableOpacity`
     font-size: 26px;
     margin-top: 50px;
     padding: 10px;
   `;
   const ButtonText = styled.Text`
+    border: 5px solid grey;
+    border-radius: 5px;
     font-family: 'Montserrat-Regular';
     font-size: 25px;
     margin: 35px;
     padding: 10px;
+    text-align: center;
+  `;
+  const Header = styled.Text`
     border: 5px solid grey;
     border-radius: 5px;
+    font-family: 'Montserrat-Regular';
+    font-size: 35px;
+    margin: 35px;
+    margin-top: 70px;
+    padding: 10px;
     text-align: center;
   `;
 
