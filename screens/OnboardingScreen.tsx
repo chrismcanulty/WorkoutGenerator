794ac17e00437de538da09945814b6a00fe0ad90
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-03-24 22:00:16 -0700

    removed safeareaview to allow title, subtitle and image components to load

--- screens/OnboardingScreen.tsx
+++ screens/OnboardingScreen.tsx
@@ -10,6 +10,7 @@ export default function OnboardingScreen({navigation}: NativeStackHeaderProps) {
   const Header = styled.Text`
     font-family: 'Montserrat-Regular';
   `;
+
   const clearOnboarding = async () => {
     try {
       await AsyncStorage.setItem('@isOnboarding', 'false');
@@ -19,55 +20,49 @@ export default function OnboardingScreen({navigation}: NativeStackHeaderProps) {
   };
 
   return (
-    <SafeAreaView>
-      <Onboarding
-        pages={[
-          {
-            backgroundColor: '#fff',
-            image: (
-              <Image
-                source={{
-                  uri: 'https://previews.123rf.com/images/irfanfirdaus/irfanfirdaus2003/irfanfirdaus200300016/143492365-vector-illustration-mobile-online-shopping-women-shop-online-with-smartphone-mobile-shopping-concept.jpg',
-                }}
-              />
-            ),
-            title: 'Onboarding',
-            subtitle: 'Done with React Native Onboarding Swiper',
-          },
-          {
-            backgroundColor: '#fff',
-            image: (
-              <Image
-                source={{
-                  uri: 'https://cdn.dribbble.com/users/1458982/screenshots/4291206/sign-in-dribble.png?compress=1&resize=400x300&vertical=top',
-                }}
-              />
-            ),
-            title: 'Onboarding',
-            subtitle: 'Done with React Native Onboarding Swiper',
-          },
-          {
-            backgroundColor: '#fff',
-            image: (
-              <Image
-                source={{
-                  uri: 'https://thumbs.dreamstime.com/b/woman-shopping-sales-happy-young-holding-paper-bags-enjoying-126694001.jpg',
-                }}
-              />
-            ),
-            title: 'Onboarding',
-            subtitle: 'Done with React Native Onboarding Swiper',
-          },
-        ]}
-      />
-      {/* <View>
-        <Header className="text-blue-600 text-3xl">
-          I am onboarding screen
-        </Header>
-        <TouchableOpacity onPress={clearOnboarding}>
-          <Text>Clear Onboarding</Text>
-        </TouchableOpacity>
-      </View> */}
-    </SafeAreaView>
+    <Onboarding
+      // onDone={() => clearOnboarding}
+      pages={[
+        {
+          backgroundColor: '#fff',
+          image: (
+            <Image
+              source={{
+                uri: 'https://previews.123rf.com/images/irfanfirdaus/irfanfirdaus2003/irfanfirdaus200300016/143492365-vector-illustration-mobile-online-shopping-women-shop-online-with-smartphone-mobile-shopping-concept.jpg',
+              }}
+              className="w-72 h-72 object-contain"
+            />
+          ),
+          title: 'Onboarding',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+        {
+          backgroundColor: '#fff',
+          image: (
+            <Image
+              source={{
+                uri: 'https://cdn.dribbble.com/users/1458982/screenshots/4291206/sign-in-dribble.png?compress=1&resize=400x300&vertical=top',
+              }}
+              className="w-72 h-72 object-contain"
+            />
+          ),
+          title: 'Onboarding',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+        {
+          backgroundColor: '#fff',
+          image: (
+            <Image
+              source={{
+                uri: 'https://thumbs.dreamstime.com/b/woman-shopping-sales-happy-young-holding-paper-bags-enjoying-126694001.jpg',
+              }}
+              className="w-72 h-72 object-contain"
+            />
+          ),
+          title: 'Onboarding',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+      ]}
+    />
   );
 }
