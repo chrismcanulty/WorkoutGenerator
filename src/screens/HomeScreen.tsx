0959f08fe85e7834d80603f35b810e5a9e303c5a
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-03-28 12:57:49 -0700

    added file with question data, minor styling in preparation for flatlist of muscle group data from api call

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,32 +1,32 @@
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
-import {Text, View} from 'react-native';
-import React, {useState} from 'react';
+import {View} from 'react-native';
+import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import styled from 'styled-components/native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import questions from '../utils/questions';
 
 export default function HomeScreen({navigation}: NativeStackHeaderProps) {
   const Button = styled.TouchableOpacity`
     font-size: 26px;
-    margin-top: 50px;
+    margin-top: 5px;
     padding: 10px;
   `;
   const ButtonText = styled.Text`
-    border: 5px solid grey;
-    border-radius: 5px;
+    border: 7px solid grey;
+    border-radius: 30px;
     font-family: 'Montserrat-Regular';
     font-size: 25px;
-    margin: 35px;
+    margin: 10px;
     padding: 10px;
     text-align: center;
   `;
   const Header = styled.Text`
-    border: 5px solid grey;
-    border-radius: 5px;
+    border: 7px solid grey;
+    border-radius: 30px;
     font-family: 'Montserrat-Regular';
-    font-size: 35px;
-    margin: 35px;
-    margin-top: 70px;
+    font-size: 24px;
+    margin: 20px;
+    margin-top: 90px;
     padding: 10px;
     text-align: center;
   `;
@@ -38,17 +38,23 @@ export default function HomeScreen({navigation}: NativeStackHeaderProps) {
       console.log('Error @clearOnboarding: ', err);
     }
   };
-
   return (
     <View>
-      <Header>Workout Generator</Header>
-      {/* replace navigation with modal? */}
-      <Button onPress={() => navigation.navigate('MuscleGroup')}>
-        <ButtonText>Generate</ButtonText>
+      <Header>{questions[1]}</Header>
+      <Button
+        style={{marginBottom: 0, padding: 0}}
+        onPress={() => navigation.navigate('MuscleGroup')}>
+        <ButtonText style={{marginTop: 220, marginBottom: 0, padding: 0}}>
+          Generate
+        </ButtonText>
       </Button>
-      <TouchableOpacity onPress={clearOnboarding}>
-        <Text>Clear onboarding</Text>
-      </TouchableOpacity>
+      <Button
+        style={{marginTop: 0, marginBottom: 0, padding: 0}}
+        onPress={clearOnboarding}>
+        <ButtonText style={{marginTop: 0, marginBottom: 0, padding: 0}}>
+          Clear onboarding
+        </ButtonText>
+      </Button>
     </View>
   );
 }
---
+++ src/utils/questions.ts
@@ -0,0 +1,7 @@
+const questions = {
+  '1': 'What muscle group do you want to target?',
+  '2': 'What equipment do you have?',
+  '3': 'How many exercises do you want to do?',
+};
+
+export default questions;
