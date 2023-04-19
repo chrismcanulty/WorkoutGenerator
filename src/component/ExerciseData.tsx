9f8ee6472b508ef1852229ab36f9b8df67f46849
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-04-19 12:57:19 -0700

    per PR comment refactored so icon is a reusable component

--- src/component/ExerciseData.tsx
+++ src/component/ExerciseData.tsx
@@ -2,9 +2,7 @@ import React, {ReactNode, useState} from 'react';
 import {ExerciseSet} from '../../types/data';
 import styled from 'styled-components/native';
 import {DataTable} from 'react-native-paper';
-import CompleteIcon from 'react-native-vector-icons/FontAwesome5';
-import EditIcon from 'react-native-vector-icons/FontAwesome5';
-import DeleteIcon from 'react-native-vector-icons/FontAwesome5';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const ExerciseSetText = styled.Text`
@@ -14,6 +12,10 @@ const ExerciseSetText = styled.Text`
   text-align: center;
   padding-bottom: 1px;
 `;
+
+const WorkoutIcon = ({name}: {name: string}) => {
+  return <Icon name={name} size={14} color={'rgb(169,169,169)'} />;
+};
 
 export default function ExerciseData({
   item,
@@ -42,21 +44,17 @@ export default function ExerciseData({
       </DataTable.Cell>
       <DataTable.Cell>
         <TouchableOpacity onPress={onPress}>
-          <CompleteIcon
-            name={completeIcon}
-            size={14}
-            color={'rgb(169,169,169)'}
-          />
+          <WorkoutIcon name={completeIcon} />
         </TouchableOpacity>
       </DataTable.Cell>
       <DataTable.Cell>
         <TouchableOpacity>
-          <EditIcon name="edit" size={14} color={'rgb(169,169,169)'} />
+          <WorkoutIcon name="edit" />
         </TouchableOpacity>
       </DataTable.Cell>
       <DataTable.Cell>
         <TouchableOpacity>
-          <DeleteIcon name="trash" size={14} color={'rgb(169,169,169)'} />
+          <WorkoutIcon name="trash" />
         </TouchableOpacity>
       </DataTable.Cell>
     </DataTable.Row>
