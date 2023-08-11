7b3729b4f5ecdc448653be5a714a408d610b2983
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-08-11 15:20:02 -0700

    splash screen now functioning in android

--- android/app/src/main/java/com/workoutgenerator/SplashActivity.java
+++ android/app/src/main/java/com/workoutgenerator/SplashActivity.java
@@ -6,34 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.splashScreenTheme);
         super.onCreate(savedInstanceState);
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-        Thread timer= new Thread()
-        {
-            public void run()
-            {
-                try
-                {
-                    //Display for 3 seconds
-                    sleep(3000);
-                }
-                catch (InterruptedException e)
-                {
-                    // TODO: handle exception
-                    e.printStackTrace();
-                }
-                finally
-                {
-                    //Goes to Activity  StartingPoint.java(STARTINGPOINT)
-                    Intent openstartingpoint=new Intent("x.y.z.START");
-                    startActivity(openstartingpoint);
-                }
-            }
-        };
-        timer.start();
         finish();
     }
 }
