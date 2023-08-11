d9b3470592fca4d432a0523fa02aeef982c85387
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-08-11 16:16:21 -0700

    transparent theme so app loads directly into splash screen without delay

--- android/app/src/main/java/com/workoutgenerator/MainActivity.java
+++ android/app/src/main/java/com/workoutgenerator/MainActivity.java
@@ -9,12 +9,6 @@ import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);
-    super.onCreate(savedInstanceState);
-  }
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -22,6 +16,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "WorkoutGenerator";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 
   /**
